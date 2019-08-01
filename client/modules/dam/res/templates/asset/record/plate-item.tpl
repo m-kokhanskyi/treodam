97167d517680a19530bc561494b896f8aa22daec
y.kondratskyi <y.kondratskyi@zinitsolutions.com>
2019-08-01 17:59:08 +0300

    added mass actions and sort for plate view

--- client/modules/dam/res/templates/asset/record/plate-item.tpl
+++ client/modules/dam/res/templates/asset/record/plate-item.tpl
@@ -8,6 +8,9 @@
 		<img src="?entryPoint=preview&size=small&id={{model.id}}&v={{version}}" alt="">
 	</div>
 	<div class="field-name">
+		<span class="record-checkbox-container">
+			<input type="checkbox" class="record-checkbox" data-id="{{model.id}}">
+		</span>
 		<a href="#{{model.name}}/view/{{model.id}}" class="link" data-id="{{model.id}}" title="{{model.attributes.name}}">{{model.attributes.name}}</a>
 	</div>
 </div>
@@ -18,6 +21,9 @@
 		height: 200px;
 		border: 1px solid #e8eced;
 		position: relative;
+	}
+	.plate-item.active {
+		background-color: #e8eced
 	}
 	.plate-item .field-type {
 		display: inline-block;
@@ -41,7 +47,7 @@
 		position: absolute;
 		bottom: 10px;
 		left: 0;
-		padding: 0 10px;
+		padding: 0 10px 0 25px;
 		text-align: center;
 		white-space: nowrap;
 		overflow: hidden;
@@ -58,6 +64,9 @@
 	.plate-item .field-preview img {
 		max-height: 100%;
 		max-width: 100%;
+		background-image: linear-gradient(45deg, #cccccc 25%, transparent 25%), linear-gradient(-45deg, #cccccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #cccccc 75%), linear-gradient(-45deg, transparent 75%, #cccccc 75%);
+		background-size: 20px 20px;
+		background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
 	}
 	.plate-item .actions {
 		color: #999999;
@@ -65,5 +74,14 @@
 		position: absolute;
 		top: 6px;
 		right: 0;
+	}
+	.plate-item .record-checkbox-container {
+		display: inline-block;
+		position: absolute;
+		left: 10px;
+		top: 0;
+	}
+	.plate-item .record-checkbox-container input {
+		cursor: pointer;
 	}
 </style>
--- client/modules/dam/src/views/asset/record/plate.js
+++ client/modules/dam/src/views/asset/record/plate.js
@@ -26,7 +26,7 @@ Espo.define('dam:views/asset/record/plate', 'views/record/list',
 
         name: 'plate',
 
-        listContainerEl: '.list > .row > .plate > .row',
+        listContainerEl: '.list > div > .plate > .row',
 
         events: _.extend({
             'click .item-container': function (e) {
@@ -40,8 +40,43 @@ Espo.define('dam:views/asset/record/plate', 'views/record/list',
 
                     this.actionQuickView({id});
                 }
+            },
+            'click [data-action="sortByDirection"]': function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+                this.sortByDirection();
+            },
+            'click [data-action="sortByField"]': function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+                this.sortByField($(e.currentTarget).data('name'));
             }
         }, Dep.prototype.events),
+
+        data() {
+            return _.extend({
+                sortFields: this.getSortFieldsList()
+            }, Dep.prototype.data.call(this));
+        },
+
+        getSortFieldsList() {
+            const fields = [];
+            const fieldDefs = this.getMetadata().get(['entityDefs', this.scope, 'fields']);
+            for (let field in fieldDefs) {
+                if (!fieldDefs[field].disabled
+                    && !fieldDefs[field].layoutListDisabled
+                    && !this.getMetadata().get(['fields', fieldDefs[field].type, 'notSortable'])
+                ) {
+                    fields.push(field);
+                }
+            }
+
+            fields.sort(function (v1, v2) {
+                return this.translate(v1, 'fields', this.scope).localeCompare(this.translate(v2, 'fields', this.scope));
+            }.bind(this));
+
+            return fields;
+        },
 
         buildRow(i, model, callback) {
             const key = model.id;
@@ -73,12 +108,67 @@ Espo.define('dam:views/asset/record/plate', 'views/record/list',
         },
 
         getSelectAttributeList(callback) {
-            callback(['name', 'code', 'private', 'code', 'type', 'fileType']);
+            callback(['name', 'code', 'private', 'code', 'type', 'fileType', 'modifiedAt']);
         },
 
         getRowContainerHtml(id) {
             return `<div class="col-xs-6 col-sm-3 item-container" data-id="${id}"></div>`;
         },
+
+        checkRecord(id, $target, isSilent) {
+            Dep.prototype.checkRecord.call(this, id, $target, isSilent);
+
+            $target = $target || this.$el.find('.record-checkbox[data-id="' + id + '"]');
+            if ($target.get(0)) {
+                $target.closest('.plate-item').addClass('active');
+            }
+        },
+
+        uncheckRecord(id, $target, isSilent) {
+            Dep.prototype.uncheckRecord.call(this, id, $target, isSilent);
+
+            $target = $target || this.$el.find('.record-checkbox[data-id="' + id + '"]');
+            if ($target.get(0)) {
+                $target.closest('.plate-item').removeClass('active');
+            }
+        },
+
+        selectAllHandler(isChecked) {
+            Dep.prototype.selectAllHandler.call(this, isChecked);
+
+            var plateItems = this.$el.find('.list .plate-item');
+            if (isChecked) {
+                plateItems.addClass('active');
+            } else {
+                plateItems.removeClass('active');
+            }
+        },
+
+        selectAllResult() {
+            Dep.prototype.selectAllResult.call(this);
+
+            this.$el.find('.list .plate-item').addClass('active');
+        },
+
+        sortByDirection() {
+            this.toggleSort(this.collection.sortBy);
+        },
+
+        sortByField(field) {
+            var asc = this.collection.asc;
+
+            this.notify('Please wait...');
+            this.collection.once('sync', function () {
+                this.notify(false);
+                this.trigger('sort', {sortBy: field, asc: asc});
+            }, this);
+            var maxSizeLimit = this.getConfig().get('recordListMaxSizeLimit') || 200;
+            while (this.collection.length > maxSizeLimit) {
+                this.collection.pop();
+            }
+            this.collection.sort(field, asc);
+            this.deactivate();
+        }
 
     })
 );

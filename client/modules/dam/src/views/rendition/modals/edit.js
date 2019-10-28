f575b44a3aa82b3fd6351e6fe59e3f8fda1834e6
d.sokolov <d.sokolov@treolabs.com>
2019-10-28 15:44:39 +0200

    fix renditons

--- client/modules/dam/src/views/rendition/modals/edit.js
+++ client/modules/dam/src/views/rendition/modals/edit.js
@@ -10,21 +10,48 @@ Espo.define('dam:views/rendition/modals/edit', 'views/modals/edit',
                     this.getMetadata().get(['clientDefs', model.name, 'recordViews', 'editSmall']) ||
                     this.getMetadata().get(['clientDefs', model.name, 'recordViews', 'editQuick']) ||
                     'views/record/edit-small';
+            let assetModel = {};
             
-            var options = {
-                model          : model,
-                el             : this.containerSelector + ' .edit-container',
-                type           : 'editSmall',
-                layoutName     : this.layoutName || 'detailSmall',
-                columnCount    : this.columnCount,
-                buttonsDisabled: true,
-                sideDisabled   : this.sideDisabled,
-                bottomDisabled : this.bottomDisabled,
-                assetModel     : this.options.relate.model,
-                exit           : function () {}
-            };
-            this.handleRecordViewOptions(options);
-            this.createView('edit', viewName, options, callback);
+            if ( this.options.relate) {
+                assetModel = this.options.relate.model;
+    
+                var options = {
+                    model          : model,
+                    el             : this.containerSelector + ' .edit-container',
+                    type           : 'editSmall',
+                    layoutName     : this.layoutName || 'detailSmall',
+                    columnCount    : this.columnCount,
+                    buttonsDisabled: true,
+                    sideDisabled   : this.sideDisabled,
+                    bottomDisabled : this.bottomDisabled,
+                    assetModel     : assetModel || {},
+                    exit           : function () {}
+                };
+                this.handleRecordViewOptions(options);
+                this.createView('edit', viewName, options, callback);
+            } else {
+                this.getModelFactory().create("Asset", asset => {
+                    asset.id = this.model.get("assetId");
+                    asset.fetch().then(() => {
+                        assetModel = asset;
+    
+                        var options = {
+                            model          : model,
+                            el             : this.containerSelector + ' .edit-container',
+                            type           : 'editSmall',
+                            layoutName     : this.layoutName || 'detailSmall',
+                            columnCount    : this.columnCount,
+                            buttonsDisabled: true,
+                            sideDisabled   : this.sideDisabled,
+                            bottomDisabled : this.bottomDisabled,
+                            assetModel     : assetModel || {},
+                            exit           : function () {}
+                        };
+                        this.handleRecordViewOptions(options);
+                        this.createView('edit', viewName, options, callback);
+                    });
+                })
+            }
         }
     })
 );

b83e72723e9ef095cb6df2be8be23d0114f33314
Dmytro Talko <d.talko@treolabs.com>
2019-11-11 16:55:32 +0200

    79209 lock entities

--- app/Controllers/Rendition.php
+++ app/Controllers/Rendition.php
@@ -2,6 +2,18 @@
 
 namespace Dam\Controllers;
 
+use Espo\Core\Exceptions\NotFound;
+
 class Rendition extends \Espo\Core\Templates\Controllers\Base
 {
+    /**
+     * @param $params
+     * @param $data
+     * @param $request
+     * @throws NotFound
+     */
+    public function actionList($params, $data, $request)
+    {
+        throw new NotFound();
+    }
 }

37010dfb7ada7fee3abcad4091dc51dafc13b854
d.sokolov <d.sokolov@treolabs.com>
2019-11-25 12:22:36 +0200

    fix show edit and show modal

--- client/modules/dam/src/views/rendition/fields/type.js
+++ client/modules/dam/src/views/rendition/fields/type.js
@@ -1,22 +1,25 @@
 Espo.define('dam:views/rendition/fields/type', ['views/fields/enum', 'dam:config'],
     (Dep, Config) => Dep.extend({
-        damConfig : null,
+        damConfig: null,
         
-        setup () {
+        setup() {
             this.damConfig = Config.prototype.init.call(this);
             Dep.prototype.setup.call(this);
         },
         
         setupOptions() {
+            if (!this.model.get("assetId")) {
+                return;
+            }
             this.wait(true);
             this.getModelFactory().create("Asset", model => {
                 model.id = this.model.get("assetId");
                 model.fetch().then(() => {
-                    let type            = this.damConfig.getType(model.get("type"));
-                    let enableTypes     = this.damConfig.getByType(`${type}.renditions`);
+                    let type        = this.damConfig.getType(model.get("type"));
+                    let enableTypes = this.damConfig.getByType(`${type}.renditions`);
                     
                     let params = [];
-                   
+                    
                     for (let i in enableTypes) {
                         let item = enableTypes[i];
                         if (!item.auto) {

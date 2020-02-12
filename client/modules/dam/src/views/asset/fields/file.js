0a07b6ad617900d1e6c1e16b019f4a0763fc2c1f
d.sokolov <d.sokolov@treolabs.com>
2020-02-12 12:48:38 +0200

    download inline files in asset

--- client/modules/dam/src/views/asset/fields/file.js
+++ client/modules/dam/src/views/asset/fields/file.js
@@ -19,7 +19,16 @@
 
 Espo.define('dam:views/asset/fields/file', 'dam:views/fields/file',
     Dep => Dep.extend({
-        setup () {
+        
+        getDownloadUrl(id) {
+            var url = this.getBasePath() + '?entryPoint=download&showInline=false&id=' + id;
+            if (this.getUser().get('portalId')) {
+                url += '&portalId=' + this.getUser().get('portalId');
+            }
+            return url;
+        },
+        
+        setup() {
             Dep.prototype.setup.call(this);
             
             this.listenTo(this.model, "change:type", () => {
